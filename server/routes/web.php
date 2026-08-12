41a3ddd8408b697c3cf1f399090913e49549fbbf
code-lova <ebinuga@hotmail.com>
2026-08-12 13:53:37 +0100

    update on web.php

--- server/routes/web.php
+++ server/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -21,23 +20,6 @@ Route::get('/', function () {
 
 Route::get('/up', function () {
     return response()->json(['status' => 'ok']);
-});
-
-// Temporary one-time endpoint to run PortfolioDataSeeder without shell access.
-// Remove this route once the data has been imported.
-Route::get('/internal/seed-portfolio-data/{token}', function (string $token) {
-    $expected = env('SEED_TOKEN');
-
-    if (!$expected || !hash_equals($expected, $token)) {
-        abort(404);
-    }
-
-    Artisan::call('db:seed', ['--class' => 'PortfolioDataSeeder', '--force' => true]);
-
-    return response()->json([
-        'status' => 'done',
-        'output' => Artisan::output(),
-    ]);
 });
 
 Auth::routes(['verify' => true]);
